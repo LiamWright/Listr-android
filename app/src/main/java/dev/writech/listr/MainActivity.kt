017ef17b706f99a564ed277b541405024c99cc58
Liam Wright <mail@liam-wright.co.uk>
2019-06-17 21:49:07 +0100

    Basic logic added

--- app/src/main/java/dev/writech/listr/MainActivity.kt
+++ app/src/main/java/dev/writech/listr/MainActivity.kt
@@ -2,11 +2,24 @@ package dev.writech.listr
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var Meals = arrayOf("one","two","three","four")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        pickMealButton.setOnClickListener{
+            Log.d("Info","Hello")
+            //Pick from array/List
+            //Display
+
+
+        }
+
+
     }
+
 }
